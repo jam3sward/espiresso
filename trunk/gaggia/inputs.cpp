fe926dc730291ffaac0aa47ff0ae8db3f84df832
james <james@52397706-df87-ed2d-b16a-be21ff91fd64>
2014-02-11 23:18:01 +0000

    Added support for button inputs (not yet working, assumed due to clash with BCM IO functions).

--- trunk/gaggia/inputs.cpp
+++ trunk/gaggia/inputs.cpp
@@ -1,13 +1,31 @@
 #include "inputs.h"
-#include "gpio.h"
 #include "settings.h"
 
 //-----------------------------------------------------------------------------
 
-Inputs::Inputs()
+Inputs::Inputs() :
+	m_button1( BUTTON1_PIN ),
+	m_button2( BUTTON2_PIN )
 {
-	BCM::open();
-	//INP_GPIO(HALTBUTTONPIN);
+	m_button1.setOutput( false );
+	m_button2.setOutput( false );
+}
+
+//-----------------------------------------------------------------------------
+
+Inputs::~Inputs()
+{
+}
+
+//-----------------------------------------------------------------------------
+
+bool Inputs::getButton( int button ) const
+{
+	switch ( button ) {
+	case 1: return m_button1.getState(); break;
+	case 2: return m_button2.getState(); break;
+	}
+	return false;
 }
 
 //-----------------------------------------------------------------------------
--- trunk/gaggia/inputs.h
+++ trunk/gaggia/inputs.h
@@ -3,11 +3,25 @@
 
 //-----------------------------------------------------------------------------
 
+#include "gpiopin.h"
+
+//-----------------------------------------------------------------------------
+
 /// Represents the inputs
 class Inputs {
 public:
 	/// Default constructor
 	Inputs();
+
+	/// Destructor
+	virtual ~Inputs();
+
+	/// Get button state (given button index, starting from 1)
+	bool getButton( int button ) const;
+
+private:
+	GPIOPin	m_button1;	///< GPIO pin for button 1
+	GPIOPin m_button2;	///< GPIO pin for button 2
 };
 
 //-----------------------------------------------------------------------------

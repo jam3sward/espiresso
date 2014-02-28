3c2f4a74f5b3cee0535070e908ffe5b9e30d6d22
jam3sward@googlemail.com <jam3sward@googlemail.com@52397706-df87-ed2d-b16a-be21ff91fd64>
2014-02-28 22:45:17 +0000

    Started refactoring temperature control loop into a separate Regulator class. This will eventually allow the user interface thread to run independently.

---
+++ trunk/gaggia/regulator.cpp
@@ -0,0 +1,150 @@
+#include "regulator.h"
+#include "timing.h"
+
+//-----------------------------------------------------------------------------
+
+Regulator::Regulator() :
+	m_run( true ),
+	m_power( false ),
+	m_timeStep( 1.0 ),
+	m_targetTemp( 95.0 ),
+	m_latestTemp( 20.0 ),
+	m_thread( &Regulator::worker, this )
+{
+}
+
+//-----------------------------------------------------------------------------
+
+Regulator::~Regulator()
+{
+	// gracefully terminate the thread
+	m_run = false;
+
+	// wait for the thread to terminate
+	m_thread.join();
+}
+
+//-----------------------------------------------------------------------------
+
+Regulator & Regulator::setPIDGains( double pGain, double iGain, double dGain )
+{
+	std::lock_guard<std::mutex> lock( m_mutex );
+	baseClass::setPIDGains( pGain, iGain, dGain );
+	return *this;
+}
+
+//-----------------------------------------------------------------------------
+
+Regulator & Regulator::setIntegratorLimits( double iMin, double iMax )
+{
+	std::lock_guard<std::mutex> lock( m_mutex );
+	baseClass::setIntegratorLimits( iMin, iMax );
+	return *this;
+}
+
+//-----------------------------------------------------------------------------
+
+Regulator & Regulator::setTimeStep( double timeStep )
+{
+	std::lock_guard<std::mutex> lock( m_mutex );
+	m_timeStep = timeStep;
+	return *this;
+}
+
+//-----------------------------------------------------------------------------
+
+Regulator & Regulator::setTargetTemperature( double target )
+{
+	std::lock_guard<std::mutex> lock( m_mutex );
+	m_targetTemp = target;
+	return *this;
+}
+
+//-----------------------------------------------------------------------------
+
+double Regulator::getTargetTemperature() const
+{
+	// the worker never modifies this
+	return m_targetTemp;
+}
+
+//-----------------------------------------------------------------------------
+
+double Regulator::getTemperature() const
+{
+	std::lock_guard<std::mutex> lock( m_mutex );
+	return m_latestTemp;
+}
+
+//-----------------------------------------------------------------------------
+
+Regulator & Regulator::setPower( bool power )
+{
+	std::lock_guard<std::mutex> lock( m_mutex );
+	m_power = power;
+	return * this;
+}
+
+//-----------------------------------------------------------------------------
+
+void Regulator::worker()
+{
+	// initialise digital thermometer
+	m_temperature.getDegrees(0);	// todo: error handling
+
+	// start time and next time step
+	double start = getClock();
+	double next  = start;
+
+	// run the thread until requested to stop
+	while (m_run) {
+		// get elapsed time since start
+		double elapsed = getClock() - start;
+
+		// take temperature measurement
+		double latestTemp = 0.0;
+		m_temperature.getDegrees( &latestTemp );
+
+		// boiler drive (duty cycle)
+		double drive = 0.0;
+
+		{
+			// lock shared data before use
+			std::lock_guard<std::mutex> lock( m_mutex );
+
+			// calculate next time step
+			next += m_timeStep;
+
+			// calculate PID update
+			drive = update( m_targetTemp - latestTemp, latestTemp );
+
+			// disable boiler if power is off
+			if ( !m_power ) drive = 0.0;
+		}
+
+		// clamp the output power to sensible range
+		if ( drive > 1.0 )
+			drive = 1.0;
+		else if ( drive < 0.0 )
+			drive = 0.0;
+
+		// set the boiler power (uses pulse width modulation)
+		m_boiler.setPower( drive );
+
+		// store the latest temperature reading
+		{
+			std::lock_guard<std::mutex> lock( m_mutex );
+			m_latestTemp = latestTemp;
+		}
+
+		// sleep for the remainder of the time step
+		double remain = next - getClock();
+		if ( remain > 0.0 )
+			delayms( static_cast<int>(1.0E3 * remain) );
+	};
+
+	// ensure the boiler is turned off before exit
+	m_boiler.powerOff();
+}
+
+//-----------------------------------------------------------------------------

fb02614f3d5e802c318c1389e634fb71c36224c9
jam3sward@googlemail.com <jam3sward@googlemail.com@52397706-df87-ed2d-b16a-be21ff91fd64>
2014-04-21 15:00:11 +0000

    Now using TSIC 306 temperature sensor, via kernel driver.

--- trunk/gaggia/temperature.cpp
+++ trunk/gaggia/temperature.cpp
@@ -9,7 +9,7 @@ using namespace std;
 
 Temperature::Temperature()
 {
-	// attempt to find the sensor on the W1 bus
+	// attempt to find the sensor path on the filesystem
 	m_sensorPath = findSensorPath();
 }
 
@@ -17,22 +17,15 @@ Temperature::Temperature()
 
 bool Temperature::getDegrees( double *value ) const
 {
-	// attempt to open the sensor
-	ifstream sensor( m_sensorPath.c_str() );
+    // attempt to open the sensor
+    ifstream sensor( m_sensorPath.c_str() );
 
-	string line;
-	getline( sensor, line );	// discard first line
-	getline( sensor, line );	// contains temperature at end, e.g. t=12345
+    // read the first line
+    string line;
+    getline( sensor, line );
 
-	// if we don't find "t=" then something is wrong
-	size_t pos = line.find("t=");
-	if ( pos == string::npos ) return false;
-
-	// extract just the number, example: 12345
-	line = line.substr( pos+2 );
-
-	// convert to degrees
-	double degrees = static_cast<double>( atoi(line.c_str()) ) / 1000.0;
+    // convert to degrees
+    double degrees = static_cast<double>( atoi(line.c_str()) ) / 1000.0;
 
 	if ( value != 0 ) *value = degrees;
 
@@ -43,22 +36,8 @@ bool Temperature::getDegrees( double *value ) const
 
 std::string Temperature::findSensorPath()
 {
-	// open file containing list of W1 slaves
-	ifstream slaves(
-		"/sys/bus/w1/devices/w1_bus_master1/w1_master_slaves"
-	);
-
-	// attempt to read first line of file: the name of the first slave
-	string firstSlave;
-	if ( !getline( slaves, firstSlave ) ) return string();
-
-	// construct full path
-	string fullPath(
-		string("/sys/bus/w1/devices/") + firstSlave + "/w1_slave"
-	);
-
-	// return to caller
-	return fullPath;
+    /* Now we use the TSIC 306, the sensor path is fixed (currently) */
+	return "/sys/kernel/tsic/temp";
 }
 
 //-----------------------------------------------------------------------------

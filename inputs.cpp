1a5663cab3bb51845c15f74504291d460e7cbfc8
jam3sward@googlemail.com <jam3sward@users.noreply.github.com>
2014-04-21 20:57:27 +0000

    Added notification callback to Inputs class.

--- inputs.cpp
+++ inputs.cpp
@@ -1,20 +1,29 @@
+#include <array>
+#include <future>
 #include "inputs.h"
 #include "settings.h"
+#include "timing.h"
 
 //-----------------------------------------------------------------------------
 
 Inputs::Inputs() :
 	m_button1( BUTTON1_PIN ),
-	m_button2( BUTTON2_PIN )
+	m_button2( BUTTON2_PIN ),
+    m_notifyFunc( nullptr ),
+    m_run( true ),
+    m_thread( &Inputs::worker, this )
 {
-	m_button1.setOutput( false );
-	m_button2.setOutput( false );
 }
 
 //-----------------------------------------------------------------------------
 
 Inputs::~Inputs()
 {
+    // gracefully terminate the thread
+    m_run = false;
+
+    // wait for the thread to terminate
+    m_thread.join();
 }
 
 //-----------------------------------------------------------------------------
@@ -27,5 +36,86 @@ bool Inputs::getButton( int button ) const
 	}
 	return false;
 }
+
+//-----------------------------------------------------------------------------
+
+Inputs & Inputs::notifyRegister( NotifyFunc func )
+{
+    // set up the notification
+    std::lock_guard<std::mutex> lock( m_mutex );
+    m_notifyFunc = func;
+
+    return *this;
+}
+
+//-----------------------------------------------------------------------------
+
+Inputs & Inputs::notifyCancel()
+{
+    // clear the notification
+    std::lock_guard<std::mutex> lock( m_mutex );
+    m_notifyFunc = nullptr;
+
+    return *this;
+}
+
+//-----------------------------------------------------------------------------
+
+void Inputs::worker()
+{
+    // set up the button inputs
+    m_button1.setOutput( false );
+    m_button2.setOutput( false );
+
+    // time period for polling
+    const unsigned period = 10;
+
+    struct State {
+        bool state;
+        bool oldState;
+        double timeStamp;
+    };
+
+    // button states
+    std::array<State,2> button;
+    for (size_t i=0; i<button.size(); ++i) {
+        button[i].oldState = false;
+        button[i].timeStamp = getClock();
+    }
+
+    // poll the buttons
+    while (m_run) {
+        // sample the button state (active low: 0=down, 1=up)
+        button[0].state = !m_button1.getState();
+        button[1].state = !m_button2.getState();
+
+        // for each button
+        for (size_t i=0; i<button.size(); ++i) {
+            // if the button state has changed
+            if ( button[i].state != button[i].oldState ) {
+                // calculate elapsed time since last event
+                double now = getClock();
+                double elapsed = now - button[i].timeStamp;
+                button[i].timeStamp = now;
+
+                // fire the notification function
+                if ( m_notifyFunc ) try {
+                    std::async(
+                        std::launch::async,
+                        m_notifyFunc,
+                        i+1, button[i].state, elapsed
+                    );
+                } catch ( const std::system_error & e ) {
+                }
+            }
+
+            // store the old button state and time stamp
+            button[i].oldState = button[i].state;
+        }
+
+        // sleep for a while
+        delayms( period );
+    }
+}//worker
 
 //-----------------------------------------------------------------------------
--- inputs.h
+++ inputs.h
@@ -3,6 +3,8 @@
 
 //-----------------------------------------------------------------------------
 
+#include <thread>
+#include <mutex>
 #include "gpiopin.h"
 
 //-----------------------------------------------------------------------------
@@ -19,9 +21,30 @@ public:
 	/// Get button state (given button index, starting from 1)
 	bool getButton( int button ) const;
 
+    /// Notification function type
+    typedef std::function<void(int button, bool down, double time)> NotifyFunc;
+
+    /// Register a function to receive notifications when a button is pressed
+    /// or released. The notification function is called asynchronously from
+    /// another thread.
+    Inputs & notifyRegister( NotifyFunc func );
+
+    /// Cancel notifications
+    Inputs & notifyCancel();
+
+private:
+    /// Worker thread
+    void worker();
+
 private:
 	GPIOPin	m_button1;	///< GPIO pin for button 1
 	GPIOPin m_button2;	///< GPIO pin for button 2
+    bool    m_run;      ///< Should thread continue to run?
+
+    NotifyFunc m_notifyFunc;    ///< Notification function
+
+    /// Thread used to monitor the inputs
+    std::thread m_thread;
 };
 
 //-----------------------------------------------------------------------------
